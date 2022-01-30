92e9b54616d1afbc3bd2c33d820685579ea2da8c
ttaerrim <ltr0121@naver.com>
2022-01-31 00:08:07 +0900

    Update: velopert-react 1-22

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useMemo } from "react";
+import CreateUser from "./CreateUser";
+import UserList from "./UserList";
+import useInputs from "./hooks/useInputs";
+
+const countActiveUsers = (users) => {
+  console.log("활성 사용자 수를 세는 중......");
+  return users.filter((user) => user.active).length;
+};
+
+const initialState = {
+  inputs: {
+    username: "",
+    email: "",
+  },
+  users: [
+    {
+      id: 1,
+      username: "velopert",
+      email: "public.velopert@gmail.com",
+      active: true,
+    },
+    {
+      id: 2,
+      username: "tester",
+      email: "tester@example.com",
+      active: false,
+    },
+    {
+      id: 3,
+      username: "liz",
+      email: "liz@example.com",
+      active: false,
+    },
+  ],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "CREATE_USER":
+      return {
+        inputs: initialState.inputs,
+        users: state.users.concat(action.user),
+      };
+    case "TOGGLE_USER":
+      return {
+        ...state,
+        users: state.users.map((user) =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        ),
+      };
+    case "REMOVE_USER":
+      return {
+        ...state,
+        users: state.users.filter((user) => user.id !== action.id),
+      };
+
+    default:
+      return state;
+  }
+}
+
+export const UserDispatch = React.createContext(null);
 
 function App() {
+  const [{ username, email }, onChange, reset] = useInputs({
+    username: "",
+    email: "",
+  });
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const { users } = state;
+
+  const count = useMemo(() => countActiveUsers(users), [users]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserDispatch.Provider value={dispatch}>
+      <CreateUser />
+      <UserList users={users} />
+      <div>활성 사용자 수: {count}</div>
+    </UserDispatch.Provider>
   );
 }
 
---
+++ src/UserList.js
@@ -0,0 +1,40 @@
+import React, { useContext } from "react";
+import { UserDispatch } from "./App";
+
+const User = React.memo(function User({ user }) {
+  const dispatch = useContext(UserDispatch);
+
+  return (
+    <div>
+      <div>
+        <b
+          style={{ cursor: "pointer", color: user.active ? "green" : "black" }}
+          onClick={() => {
+            dispatch({ type: "TOGGLE_USER", id: user.id });
+          }}
+        >
+          {user.username}
+        </b>{" "}
+        <span>({user.email})</span>
+        <button
+          onClick={() => {
+            dispatch({ type: "REMOVE_USER", id: user.id });
+          }}
+        >
+          삭제
+        </button>
+      </div>
+    </div>
+  );
+});
+const UserList = ({ users }) => {
+  return (
+    <div>
+      {users.map((user) => (
+        <User user={user} key={user.id} />
+      ))}
+    </div>
+  );
+};
+
+export default React.memo(UserList);
